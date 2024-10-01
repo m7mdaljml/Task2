e98b1615a74e7f11813473401d8b236ce3610c5c
m7mdaljml <124713884+m7mdaljml@users.noreply.github.com>
2024-10-01 14:20:04 +0300

    Enhance the view

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <v-choice
-  v-model="choiceModel" 
-  name="group1" 
-  :options="options" 
-  :multiple="true"
-  @CheckItem="check"
-  label="Radio Group"/>
-
-  {{ choiceModel }}
+  <v-choice v-model="choiceModel" name="group1" :options="options" :multiple="true" @CheckItem="check"
+    label="Radio Group" />
+  <ul>
+  <li v-for="(item,index) in choiceModel" :key="index">
+    {{item}}
+  </li>
+  </ul>
+  
 </template>
 
 <script setup lang="ts">
@@ -15,30 +14,29 @@ import { ref } from 'vue';
 import VChoice from '@/components/VChoice.vue'
 
 const options = [
-  { text: "Option 1",  value: "a" },
-  { text: "Option 2",  value: "b" },
-  { text: "Option 3",  value: "c" },
+  { text: "Option 1", value: "a" },
+  { text: "Option 2", value: "b" },
+  { text: "Option 3", value: "c" },
 ]
 
 const choiceModel = ref([])
 
 const newIndex = ref('')
-const check = (item,type)=>{
-  if(type)
-{
-  if(!choiceModel.value.includes(item)){
+const check = (item, type) => {
+  if (type) {
+    if (!choiceModel.value.includes(item)) {
+      choiceModel.value.push(item)
+    }
+    else {
+      newIndex.value = choiceModel.value.indexOf(item)
+      choiceModel.value.splice(newIndex.value, 1)
+    }
+  }
+  else {
+    choiceModel.value = []
     choiceModel.value.push(item)
   }
-  else{
-    newIndex.value = choiceModel.value.indexOf(item)
-    choiceModel.value.splice(newIndex.value,1)
-  }
-}
-else{
-  choiceModel.value =[]
-  choiceModel.value.push(item)
-}
-  
+
 }
 </script>
 
